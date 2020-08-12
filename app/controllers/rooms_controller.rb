3a515e3f05e1351ce8fd41877a05581c6f0a1d37
rui4210 <69191254+rui4210@users.noreply.github.com>
2020-08-12 17:38:24 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+  
+  def index
+  end
+  
   def new
     @room = Room.new
   end
@@ -22,9 +26,6 @@ class RoomsController < ApplicationController
 
   def room_params
     params.require(:room).permit(:name, user_ids:[])
-  end
-
-  def index
   end
 
 end
